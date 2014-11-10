b57aa4395f37370d61cbdaab4ed1a90b687bfbad
Ethan Jewett <esjewett@gmail.com>
2014-11-10 08:45:58 -0600

    Dual build approach

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 var gulp = require('gulp');
 
 var browserify = require('browserify');
+var shim = require('browserify-shim');
 var source = require('vinyl-source-stream');
 
 gulp.task('scripts', function () {
 	return browserify('./src/main.js')
 		// .exclude('jquery')
+		.transform(shim)
 		.bundle()
 		.pipe(source('dependency.js'))
         .pipe(gulp.dest('./'));
 });
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('./src/**/*.js', ['scripts']);
+gulp.task('scripts-with-deps', function () {
+	return browserify('./src/main.js')
+		.bundle()
+		.pipe(source('dependency-bundle.js'))
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['scripts', 'watch']);
-gulp.task('all', ['scripts']);
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('./src/**/*.js', ['scripts', 'scripts-with-deps']);
+});
+
+gulp.task('default', ['scripts', 'scripts-with-deps', 'watch']);
+gulp.task('all', ['scripts', 'scripts-with-deps']);
